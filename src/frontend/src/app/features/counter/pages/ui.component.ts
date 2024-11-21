260ef0c002c4cddf027d3c621354a5b73b9b8c48
Jason Barney <Jason_M_Barney@progressive.com>
2024-11-21 14:27:53 -0500

    FizzBuzz

--- src/frontend/src/app/features/counter/pages/ui.component.ts
+++ src/frontend/src/app/features/counter/pages/ui.component.ts
@@ -20,6 +20,10 @@ import {
       </button>
       <span data-testid="current">{{ counter() }}</span>
       <button (click)="increment()" class="btn btn-primary">+</button>
+
+      <div>
+        <p>{{ fizzBuzz() }}</p>
+      </div>
     </div>
   `,
   styles: ``,
@@ -36,4 +40,16 @@ export class UiComponent {
   }
 
   counterAtZero = computed(() => this.counter() === 0);
+  counterDivisibleBy3 = computed(() => this.counter() % 3 === 0);
+  counterDivisibleBy5 = computed(() => this.counter() % 5 === 0);
+
+  fizzBuzz = computed(() => {
+    const current = this.counter();
+    let text = '';
+    if (!this.counterAtZero()) {
+      if (current % 3 === 0) text += 'Fizz';
+      if (current % 5 === 0) text += 'Buzz';
+    }
+    return text;
+  });
 }
