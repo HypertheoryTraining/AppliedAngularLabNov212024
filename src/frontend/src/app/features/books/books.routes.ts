f2bb18bbd2d5ba6e9d877ad9b7d14f51c9bae914
Jason Barney <Jason_M_Barney@progressive.com>
2024-11-21 15:57:26 -0500

    Remove books component

--- src/frontend/src/app/features/books/books.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { BooksComponent } from './books.component';
-
-export const BOOKS_ROUTES: Routes = [
-  {
-    path: '',
-    component: BooksComponent,
-  },
-];
