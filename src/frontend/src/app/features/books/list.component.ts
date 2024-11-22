85d95d305ae423627b8a60c424c16c563677db0a
Jimmy Gross <jimmyisnotgross@gmail.com>
2024-11-22 11:06:57 -0500

    sorting

--- src/frontend/src/app/features/books/list.component.ts
+++ src/frontend/src/app/features/books/list.component.ts
@@ -1,5 +1,5 @@
 import { JsonPipe, CommonModule } from "@angular/common";
-import { Component, ChangeDetectionStrategy, resource } from "@angular/core";
+import { Component, ChangeDetectionStrategy, resource, signal, computed } from "@angular/core";
 
 export type BookEntity = {
   author: string;
@@ -22,14 +22,14 @@ export type BookEntity = {
       <table class="table w-full">
         <thead>
           <tr>
-            <th>ID</th>
-            <th>Title</th>
-            <th>Author</th>
-            <th>Year</th>
+            <th (click)="sort('id')">ID</th>
+            <th (click)="sort('title')">Title</th>
+            <th (click)="sort('author')">Author</th>
+            <th (click)="sort('year')">Year</th>
           </tr>
         </thead>
         <tbody>
-          <tr *ngFor="let book of books.value()">
+          <tr *ngFor="let book of sortedBooks()">
             <td>{{ book.id }}</td>
             <td>{{ book.title }}</td>
             <td>{{ book.author }}</td>
@@ -48,4 +48,31 @@ export class ListComponent {
         .then((res) => res.json())
         .then((r) => r.data),
   });
+
+  sortColumn = signal<'id' | 'title' | 'author' | 'year'>('title');
+  sortOrder = signal<'asc' | 'desc'>('asc');
+
+  sortedBooks = computed(() => {
+    const books = this.books.value() || [];
+    const column = this.sortColumn();
+    const order = this.sortOrder();
+
+    return books.slice().sort((a, b) => {
+      const valueA = a[column];
+      const valueB = b[column];
+
+      if (valueA < valueB) return order === 'asc' ? -1 : 1;
+      if (valueA > valueB) return order === 'asc' ? 1 : -1;
+      return 0;
+    });
+  });
+
+  sort(column: 'id' | 'title' | 'author' | 'year') {
+    if (this.sortColumn() === column) {
+      this.sortOrder.set(this.sortOrder() === 'asc' ? 'desc' : 'asc');
+    } else {
+      this.sortColumn.set(column);
+      this.sortOrder.set('asc');
+    }
+  }
 }
