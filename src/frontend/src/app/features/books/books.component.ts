3903029d90324131f369ada0ce51643a689e705c
Jimmy Gross <jimmyisnotgross@gmail.com>
2024-11-22 10:15:32 -0500

    step 2!

--- src/frontend/src/app/features/books/books.component.ts
+++ src/frontend/src/app/features/books/books.component.ts
@@ -1,5 +1,5 @@
-import { JsonPipe, CommonModule } from '@angular/common';
-import { Component, ChangeDetectionStrategy, resource } from '@angular/core';
+import { JsonPipe, CommonModule } from "@angular/common";
+import { Component, ChangeDetectionStrategy, resource } from "@angular/core";
 
 export type BookEntity = {
   author: string;
@@ -18,13 +18,26 @@ export type BookEntity = {
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [CommonModule, JsonPipe],
   template: `
-    <h2>Books List</h2>
-    <ul>
-      <li *ngFor="let book of books.value()">
-        <strong>{{ book.title }}</strong> by {{ book.author }}
-      </li>
-    </ul>
-    <pre>{{ books.value() | json }}</pre>
+    <div class="overflow-x-auto">
+      <table class="table w-full">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Author</th>
+            <th>Year</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr *ngFor="let book of books.value()">
+            <td>{{ book.id }}</td>
+            <td>{{ book.title }}</td>
+            <td>{{ book.author }}</td>
+            <td>{{ book.year }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   `,
   styles: [],
 })
